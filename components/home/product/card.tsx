ea03226c422a2c9b73d96eb5619bb29b1d69b5d9
Nirdosh <bimallamixane2@gmail.com>
2024-10-03 19:12:41 +0545

    Mobile Product View Fixed

--- components/home/product/card.tsx
+++ components/home/product/card.tsx
@@ -6,9 +6,9 @@ import React from "react";
 export default function ProductsCard() {
   return (
     <Link className="my-2" href={``}>
-      <section className="h-[280px] md:h-[360px] min-w-[180px] md:min-w-[210px] pb-10 text-start transform duration-500 hover:-translate-y-2 cursor-pointer overflow-hidden border rounded">
+      <section className="h-[240px] md:h-[360px] min-w-[180px] md:min-w-[210px] pb-10 text-start transform duration-500 hover:-translate-y-2 cursor-pointer overflow-hidden border rounded">
         <Image
-          className=" h-[140px] md:h-[200px] 2xl:h-[220px] w-full object-center object-cover rounded"
+          className=" h-[140px] md:h-[200px] 2xl:h-[220px] w-full object-center object-cover"
           src={
             "https://images.unsplash.com/photo-1496226338867-3304059a5aa6?q=80&w=1446&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           }
@@ -18,14 +18,14 @@ export default function ProductsCard() {
         />
         <div className="flex flex-row justify-between items-center px-1">
           <div className="flex flex-col">
-            <h1 className="text-md md:text-lg font-medium my-3 px-5 truncate">
+            <h1 className="text-md md:text-lg font-medium my-0.5 md:my-3 px-5 truncate">
               Vastra Wears
             </h1>
-            <h2 className="text-sm md:text-lg font-medium text-gray-600  mb-3 px-5">
+            <h2 className="text-sm md:text-lg font-medium text-gray-600  my-1.5 md:mb-3 px-5">
               Rs {0.0}
             </h2>
           </div>
-          <div className="hidden md:block p-2 text-sm border-[1px] border-gray-300 bg-white text-green-500 rounded-xl">
+          <div className="hidden md:block p-1 md:p-2 text-sm border-[1px] border-gray-300 bg-white text-green-500 rounded-xl">
             In Stock
           </div>
         </div>
